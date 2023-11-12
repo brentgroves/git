799a4dd9651eae1c1b5c9bf0b499a147db91ec80
brentgroves <brent.groves@gmail.com>
2023-11-12 17:04:45 -0500

    updated source code

--- freshstart.sh
+++ freshstart.sh
@@ -2,21 +2,21 @@
 # https://gist.github.com/gitaarik/8735255
 pushd .
 cd ~
-rm ~/dotfiles
+rm -rf ~/dotfiles
 git clone https://github.com/brentgroves/dotfiles.git
 cd ~/src
 rm -rf ~/src/repsys
 git clone --recursive git@github.com:brentgroves/repsys.git
 ## git switch main if detached head
-cd ~src/repsys/git
+cd ~/src/repsys/git
 git switch main
-cd ~src/repsys/projects
+cd ~/src/repsys/projects
 git switch main
-cd ~src/repsys/status
+cd ~/src/repsys/status
 git switch main
-cd ~src/repsys/volumes/ansible/rsapb
+cd ~/src/repsys/volumes/ansible/rsapb
 git switch main
-cd ~src/repsys/volumes/bash/rsbsh
+cd ~/src/repsys/volumes/bash/rsbsh
 git switch main
 cd ~/src/repsys/volumes/go/replib
 git switch main
@@ -24,7 +24,7 @@ cd ~/src/repsys/volumes/go/runner
 git switch main
 cd ~/src/repsys/volumes/java/etlj
 git switch main
-cd ~src/repsys/volumes/pki
+cd ~/src/repsys/volumes/pki
 git switch main
 cd ~/src/repsys/volumes/python/tbetl
 git switch main
